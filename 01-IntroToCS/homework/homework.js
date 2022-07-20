3a930409d6dbdddf27103b752a0572301d2d68bc
Jesús Lautaro Careglio Albornoz <jlcareglioalbornoz@gmail.com>
2022-07-20 12:27:01 -0300

    0️⃣1️⃣ y 0️⃣2️⃣ Resueltos

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,32 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-  
+  return parseInt(num, 2);
+
+  // // Solución aplicando razonamiento lógico paso a paso:
+  // let binario = num.toString().split("").map(Number);
+  // let decimal = 0;
+  // for (let i = binario.length - 1; i >= 0; i--) {
+  //   const dígitoBinario = binario[i];
+  //   const posición = binario.length - 1 - i;
+  //   decimal += dígitoBinario * 2 ** posición;
+  // }
+  // return decimal;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  return (num >>> 0).toString(2);
 
+  // // Solución aplicando razonamiento lógico paso a paso:
+  // let binario = "";
+  // do {
+  //   binario = num % 2 + binario;
+  //   num = parseInt(num / 2);
+  // } while (num != 0);
+  // return binario;
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
