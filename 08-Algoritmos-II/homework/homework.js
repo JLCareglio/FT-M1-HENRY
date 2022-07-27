1b40bb6d0b682bf316c7e5c94852ee88c35aa2ef
Jesús Lautaro Careglio Albornoz <jlcareglioalbornoz@gmail.com>
2022-07-27 20:57:01 -0300

    🚧

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,20 +1,32 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
-  // Implementar el método conocido como quickSort para ordenar de menor a mayor
-  // el array recibido como parámetro
-  // Devolver el array ordenado resultante
-  // Tu código:
-
+  if (array.length > 1) {
+    let pivote = Math.floor(Math.random() * array.length);
+    let aL = [];
+    let aR = [];
+    let aIguales = array.splice(pivote, 1);
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < aIguales[0]) aL.push(array[i]);
+      else if (array[i] > aIguales[0]) aR.push(array[i]);
+      else aIguales.push(array[i]);
+    }
+    
+  } else return array;
 }
 
 function mergeSort(array) {
-  // Implementar el método conocido como mergeSort para ordenar de menor a mayor
-  // el array recibido como parámetro
-  // Devolver el array ordenado resultante
-  // Tu código:
-
+  if (array.length > 1) {
+    let aL = array.splice(0, array.length / 2);
+    let aR = array;
+    aL = mergeSort(aL);
+    aR = mergeSort(aR);
+    let aFinal = [];
+    while (aL.length && aR.length)
+      aFinal.push(aL[0] < aR[0] ? aL.shift() : aR.shift());
+    return aFinal.concat(aL).concat(aR);
+  } else return array;
 }
 
 // No modificar nada debajo de esta línea
